02a512c63b8edff379486322408b45bbf63f1b0f
Tomoya Hirano <tomoya.hirano@dena.com>
2015-11-11 18:58:36 +0900

    init project

---
+++ StackPageViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackPageViewDemo
+//
+//  Created by Tomoya Hirano on 2015/11/11.
+//  Copyright © 2015年 Tomoya Hirano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
